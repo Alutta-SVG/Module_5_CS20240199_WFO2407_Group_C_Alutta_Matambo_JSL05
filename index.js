e0bc013d13e339a31b5c1b3f519669ec0f4d0ab3
Alutta <Aluttamatambo5@gmail.com>
2024-09-12 22:25:01 +0200

    Added guardians

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "R&B",
+    "Rocket": "Rock",
+    "Groot": "Pop",
     // Add preferences for Drax, Rocket, and Groot
 };
 
